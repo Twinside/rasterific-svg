f23201992a1dc58d219a74c47b5fa0ba9fe5efe6
Vincent Berthoux <twinside@gmail.com>
2015-02-07 19:38:46 +0100

    Separating testing to the renderer executable.

--- exec-src/svgrender.hs
+++ exec-src/svgrender.hs
@@ -1,119 +1,115 @@
+
 {-# LANGUAGE CPP #-}
 #if !MIN_VERSION_base(4,8,0)
 import Data.Foldable( foldMap )
 #endif
 
-import Control.Applicative( (<$>) )
-import Control.Monad( forM_ )
+import Control.Applicative( (<$>), (<|>) )
+import Control.Monad( when )
 import Data.Monoid( (<>) )
-import Data.List( isSuffixOf, sort )
-import System.Environment( getArgs )
-import System.Directory( createDirectoryIfMissing
-                       , getDirectoryContents
-                       )
-import qualified Text.Blaze.Html5 as HT
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as H
-import qualified Text.Blaze.Html.Renderer.String as H
-import System.FilePath( dropExtension, (</>), (<.>), splitFileName )
-
 import Codec.Picture( writePng )
+import System.FilePath( replaceExtension )
 
-import Graphics.Text.TrueType( FontCache )
-import Graphics.Rasterific.Svg
-import Graphics.Svg hiding ( text, path )
-{-import Debug.Trace-}
-{-import Text.Printf-}
-{-import Text.Groom-}
+import Options.Applicative( Parser
+                          , ParserInfo
+                          , argument
+                          , execParser
+                          , fullDesc
+                          , header
+                          , help
+                          , helper
+                          , info
+                          , long
+                          , metavar
+                          , progDesc
+                          , str
+                          , switch
+                          , auto
+                          , option
+                          )
 
-loadRender :: [String] -> IO ()
-loadRender [] = putStrLn "not enough arguments"
-loadRender [_] = putStrLn "not enough arguments"
-loadRender (svgfilename:pngfilename:_) = do
-  f <- loadSvgFile svgfilename
-  case f of
-     Nothing -> putStrLn "Error while loading SVG"
-     Just doc -> do
-        cache <- loadCreateFontCache "fonty-texture-cache"
-        (finalImage, _) <- renderSvgDocument cache Nothing 96 doc
-        writePng pngfilename finalImage
+import Graphics.Rasterific.Svg( loadCreateFontCache
+                              , renderSvgDocument
+                              )
+import Graphics.Svg( loadSvgFile
+                   , documentSize )
+import System.Exit( ExitCode( ExitFailure, ExitSuccess )
+                  , exitWith )
 
-testOutputFolder :: FilePath
-testOutputFolder = "gen_test"
+data Options = Options
+  { _inputFile  :: !FilePath
+  , _outputFile :: !FilePath
+  , _verbose    :: !Bool
+  , _width      :: !Int
+  , _height     :: !Int
+  , _dpi        :: !Int
+  }
 
-img :: FilePath -> Int -> Int -> H.Html
-img path _w _h = H.img H.! H.src (H.toValue path)
+argParser :: Parser Options
+argParser = Options
+  <$> ( argument str
+            (metavar "SVGINPUTFILE"
+            <> help "SVG file to render to png"))
+  <*> ( argument str
+            (metavar "OUTPUTFILE"
+            <> help ("Output file name, same as input with"
+                    <> " different extension if unspecified."))
+        <|> pure "" )
+  <*> ( switch (long "verbose" <> help "Display more information") )
+  <*> ( option auto
+            (  long "width"
+            <> help "Force the width of the rendered PNG"
+            <> metavar "WIDTH" )
+        <|> pure 0
+        )
+  <*> ( option auto
+            (  long "height"
+            <> help "Force the height of the rendered PNG"
+            <> metavar "HEIGHT" )
+        <|> pure 0 )
+  <*> ( option auto
+            (  long "dpi"
+            <> help "DPI used for text rendering and various real life sizes"
+            <> metavar "DPI" )
+        <|> pure 96 )
 
-table :: [H.Html] -> [[H.Html]] -> H.Html
-table headers cells =
-  H.table $ header <> foldMap (H.tr . foldMap H.td) cells
-  where header = H.tr $ foldMap H.th headers
+progOptions :: ParserInfo Options
+progOptions = info (helper <*> argParser)
+      ( fullDesc
+     <> progDesc "Convert SVGINPUTFILE into a png OUTPUTFILE"
+     <> header "svgrender svg file renderer." )
 
-testFileOfPath :: FilePath -> FilePath
-testFileOfPath path = testOutputFolder </> base <.> "png"
-  where (_, base) = splitFileName path
+outFileName :: Options -> FilePath
+outFileName Options { _outputFile = "", _inputFile = inf } =
+    replaceExtension inf "png"
+outFileName opt = _outputFile opt
 
-svgTestFileOfPath :: FilePath -> FilePath
-svgTestFileOfPath path = testOutputFolder </> base <.> "svg"
-  where (_, base) = splitFileName path
-
-
-text :: String -> H.Html
-text txt = H.toHtml txt <> H.br
-
-generateFileInfo :: FilePath -> [H.Html]
-generateFileInfo path =
-    [ text path, img path 0 0
-    , img pngRef 0 0
-    , img (testFileOfPath path) 0 0
-    , img (svgTestFileOfPath path) 0 0]
+fixSize :: Options -> (Int, Int) -> (Int, Int)
+fixSize opt (w, h) = (notNull (_width opt) w, notNull (_height opt) h)
   where
-    pngRef = dropExtension path <.> "png"
+    notNull v v' = if v <= 0 then v' else v
 
-toHtmlDocument :: H.Html -> String
-toHtmlDocument html = H.renderHtml $
-  H.html $ H.head (HT.title $ H.toHtml "Test results")
-        <> H.body html
-                
-analyzeFolder :: FontCache -> FilePath -> IO ()
-analyzeFolder cache folder = do
-  createDirectoryIfMissing True testOutputFolder
-  fileList <- sort . filter (".svg" `isSuffixOf`) <$> getDirectoryContents folder
-  let hdr = H.toHtml <$> ["name", "W3C Svg", "W3C ref PNG", "mine", "svgmine"]
-      all_table =
-        table hdr . map generateFileInfo $ map (folder </>) fileList
-      doc = toHtmlDocument all_table
-      (_, folderBase) = splitFileName folder
+runConversion :: Options -> IO ()
+runConversion options = do
+  cache <- loadCreateFontCache "rasterific-svg-font-cache"
+  let filename = _inputFile options
+      whenVerbose = when (_verbose options) . putStrLn
+  whenVerbose $ "Loading: " ++ filename 
 
-  print fileList
+  svg <- loadSvgFile filename
+  case svg of
+    Nothing -> do
+      putStrLn $ "Failed to load " ++ filename
+      exitWith $ ExitFailure 1
 
-  writeFile (folder </> ".." </> folderBase <.> "html") doc
-  forM_ fileList $ \p -> do
-    let realFilename = folder </> p
-    putStrLn $ "Loading: " ++ realFilename
-    svg <- loadSvgFile realFilename
-    {-putStrLn $ groom svg-}
-    case svg of
-      Nothing -> putStrLn $ "Failed to load " ++ p
-      Just d -> do
-        putStrLn $ "   => Rendering " ++ show (documentSize 96 d)
-        (finalImage, _) <- renderSvgDocument cache Nothing 96 d
-        writePng (testFileOfPath p) finalImage
-
-        putStrLn "   => XMLize"
-        saveXmlFile (svgTestFileOfPath p) d
-
-
-testSuite :: IO ()
-testSuite = do
-    cache <- loadCreateFontCache "rasterific-svg-font-cache"
-    analyzeFolder cache "w3csvg"
-    analyzeFolder cache "test"
+    Just d -> do
+      let dpi = _dpi options
+          size = fixSize options $ documentSize dpi d
+      whenVerbose $ "Rendering at " ++ show size
+      (finalImage, _) <- renderSvgDocument cache (Just size) dpi d
+      writePng (outFileName options) finalImage
+      exitWith ExitSuccess
 
 main :: IO ()
-main = do
-    args <- getArgs
-    case args of
-      "test":_ -> testSuite
-      _ -> loadRender args
+main = execParser progOptions >>= runConversion
 
---
+++ exec-src/svgtest.hs
@@ -0,0 +1,119 @@
+{-# LANGUAGE CPP #-}
+#if !MIN_VERSION_base(4,8,0)
+import Data.Foldable( foldMap )
+#endif
+
+import Control.Applicative( (<$>) )
+import Control.Monad( forM_ )
+import Data.Monoid( (<>) )
+import Data.List( isSuffixOf, sort )
+import System.Environment( getArgs )
+import System.Directory( createDirectoryIfMissing
+                       , getDirectoryContents
+                       )
+import qualified Text.Blaze.Html5 as HT
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as H
+import qualified Text.Blaze.Html.Renderer.String as H
+import System.FilePath( dropExtension, (</>), (<.>), splitFileName )
+
+import Codec.Picture( writePng )
+
+import Graphics.Text.TrueType( FontCache )
+import Graphics.Rasterific.Svg
+import Graphics.Svg hiding ( text, path )
+{-import Debug.Trace-}
+{-import Text.Printf-}
+{-import Text.Groom-}
+
+loadRender :: [String] -> IO ()
+loadRender [] = putStrLn "not enough arguments"
+loadRender [_] = putStrLn "not enough arguments"
+loadRender (svgfilename:pngfilename:_) = do
+  f <- loadSvgFile svgfilename
+  case f of
+     Nothing -> putStrLn "Error while loading SVG"
+     Just doc -> do
+        cache <- loadCreateFontCache "fonty-texture-cache"
+        (finalImage, _) <- renderSvgDocument cache Nothing 96 doc
+        writePng pngfilename finalImage
+
+testOutputFolder :: FilePath
+testOutputFolder = "gen_test"
+
+img :: FilePath -> Int -> Int -> H.Html
+img path _w _h = H.img H.! H.src (H.toValue path)
+
+table :: [H.Html] -> [[H.Html]] -> H.Html
+table headers cells =
+  H.table $ header <> foldMap (H.tr . foldMap H.td) cells
+  where header = H.tr $ foldMap H.th headers
+
+testFileOfPath :: FilePath -> FilePath
+testFileOfPath path = testOutputFolder </> base <.> "png"
+  where (_, base) = splitFileName path
+
+svgTestFileOfPath :: FilePath -> FilePath
+svgTestFileOfPath path = testOutputFolder </> base <.> "svg"
+  where (_, base) = splitFileName path
+
+
+text :: String -> H.Html
+text txt = H.toHtml txt <> H.br
+
+generateFileInfo :: FilePath -> [H.Html]
+generateFileInfo path =
+    [ text path, img path 0 0
+    , img pngRef 0 0
+    , img (testFileOfPath path) 0 0
+    , img (svgTestFileOfPath path) 0 0]
+  where
+    pngRef = dropExtension path <.> "png"
+
+toHtmlDocument :: H.Html -> String
+toHtmlDocument html = H.renderHtml $
+  H.html $ H.head (HT.title $ H.toHtml "Test results")
+        <> H.body html
+                
+analyzeFolder :: FontCache -> FilePath -> IO ()
+analyzeFolder cache folder = do
+  createDirectoryIfMissing True testOutputFolder
+  fileList <- sort . filter (".svg" `isSuffixOf`) <$> getDirectoryContents folder
+  let hdr = H.toHtml <$> ["name", "W3C Svg", "W3C ref PNG", "mine", "svgmine"]
+      all_table =
+        table hdr . map generateFileInfo $ map (folder </>) fileList
+      doc = toHtmlDocument all_table
+      (_, folderBase) = splitFileName folder
+
+  print fileList
+
+  writeFile (folder </> ".." </> folderBase <.> "html") doc
+  forM_ fileList $ \p -> do
+    let realFilename = folder </> p
+    putStrLn $ "Loading: " ++ realFilename
+    svg <- loadSvgFile realFilename
+    {-putStrLn $ groom svg-}
+    case svg of
+      Nothing -> putStrLn $ "Failed to load " ++ p
+      Just d -> do
+        putStrLn $ "   => Rendering " ++ show (documentSize 96 d)
+        (finalImage, _) <- renderSvgDocument cache Nothing 96 d
+        writePng (testFileOfPath p) finalImage
+
+        putStrLn "   => XMLize"
+        saveXmlFile (svgTestFileOfPath p) d
+
+
+testSuite :: IO ()
+testSuite = do
+    cache <- loadCreateFontCache "rasterific-svg-font-cache"
+    analyzeFolder cache "w3csvg"
+    {-analyzeFolder cache "test"-}
+
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+      "test":_ -> testSuite
+      _ -> loadRender args
+
