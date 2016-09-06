f307a17cd23387db89aadd0744d80c48aa340864
Vincent Berthoux <twinside@gmail.com>
2016-09-06 23:36:08 +0200

    GradientMesh milestone

--- src/Graphics/Rasterific/Svg/MeshConverter.hs
+++ src/Graphics/Rasterific/Svg/MeshConverter.hs
@@ -6,6 +6,13 @@ import Data.Monoid( mconcat )
 import Control.Applicative( pure, (<$>) )
 #endif
 
+import Graphics.Rasterific.Linear( (^+^)
+                                 , (^-^)
+                                 , (^*)
+                                 , lerp
+                                 )
+import qualified Linear as L
+import qualified Graphics.Rasterific as R
 import Codec.Picture( PixelRGBA8 )
 
 import Graphics.Svg.Types
@@ -17,44 +24,27 @@ convert = undefined
 svgMeshSize :: MeshGradient -> (Int, Int)
 svgMeshSize mesh = (width, height) where
   height = length $ _meshGradientRows mesh
-  width = maximum $ length <$> _meshGradientRows mesh
+  width = maximum $ length . _meshGradientRowPatches <$> _meshGradientRows mesh
 
+svgPathToPrimitives :: R.Point -> R.Point -> GradientPathCommand -> R.CubicBezier
+svgPathToPrimitives firstPatchPoint = go where
+  go o GClose = o `straightLine` firstPatchPoint
+  go o (GLine OriginRelative c) = o `straightLine` (o ^+^ mp c)
+  go o (GLine OriginAbsolute p) = o `straightLine` mp p
+  go o (GCurve OriginAbsolute c1 c2 e) =
+    R.CubicBezier o (toR c1) (toR c2) (mp e)
+  go o (GCurve OriginRelative c1 c2 e) =
+    R.CubicBezier o (o ^+^ toR c1) (o ^+^ toR c2) (o ^+^ mp e)
 
-data PathConverter 
+  mp Nothing = firstPatchPoint
+  mp (Just p) = toR p
 
-svgPathToPrimitives :: Bool -> [PathCommand] -> [R.Primitive]
-svgPathToPrimitives shouldClose lst
-    | shouldClose && not (nearZero $ norm (lastPoint ^-^ firstPoint)) =
-        concat $ prims ++ [R.line lastPoint firstPoint]
-    | otherwise = concat prims
-  where
-    ((lastPoint, _, firstPoint), prims) =
-        mapAccumL go (zero, zero, zero) $ singularize lst
+toR :: RPoint -> R.Point
+{-# INLINE toR #-}
+toR (L.V2 x y) = realToFrac <$> R.V2 x y
 
-    go (latest, p, first) GClose =
-        ((first, p, first), R.line latest first)
-
-    go (_, _, _) (MoveTo OriginAbsolute (p:_)) = ((p', p', p'), [])
-      where p' = toR p
-    go (o, _, _) (MoveTo OriginRelative (p:_)) =
-        ((pp, pp, pp), []) where pp = o ^+^ toR p
-
-    go (o, _, fp) (LineTo OriginRelative (c:_)) =
-        ((p, p, fp), R.line o p) where p = o ^+^ toR c
-
-    go (o, _, fp) (LineTo OriginAbsolute (p:_)) =
-        ((p', p', fp), R.line o $ toR p)
-          where p' = toR p
-
-    go (o, _, fp) (CurveTo OriginAbsolute ((c1, c2, e):_)) =
-        ((e', c2', fp),
-            [R.CubicBezierPrim $ R.CubicBezier o (toR c1) c2' e'])
-       where e' = toR e
-             c2' = toR c2
-
-    go (o, _, fp) (CurveTo OriginRelative ((c1, c2, e):_)) =
-        ((e', c2', fp), [R.CubicBezierPrim $ R.CubicBezier o c1' c2' e'])
-      where c1' = o ^+^ toR c1
-            c2' = o ^+^ toR c2
-            e' = o ^+^ toR e
+straightLine :: R.Point -> R.Point -> R.CubicBezier
+straightLine a b = R.CubicBezier a p1 p2 b where
+  p1 = lerp (1/3) a b
+  p2 = lerp (2/3) a b
 
